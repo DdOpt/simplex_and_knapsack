787a16a5a5ab39919d1757dfbc0a7a0c18a0249e
maweizheng <15101093126@163.com>
2023-04-13 00:34:30 +0800

    output the selected patten

--- knapsack/CG.py
+++ knapsack/CG.py
@@ -1,15 +1,15 @@
 from gurobipy import *
 import numpy as np
 
-w = [1,2,2,3,4]
-capacity = 6
-#w = [np.random.randint(1,capacity-30) for i in range(capacity)]
+#w = [1,2,2,3,4]
+capacity = 60
+w = [np.random.randint(1,capacity-30) for i in range(capacity-50)]
 A = np.eye(len(w))
 b = np.ones(len(w))
 try:
     #the rmp model
     masterpro = Model("rmp")
-    y = masterpro.addMVar(len(w),obj=1,vtype=GRB.CONTINUOUS,name="y")
+    y = masterpro.addMVar(len(w),obj=1,vtype=GRB.CONTINUOUS,name="p")
     set_partition = masterpro.addMConstr(A,y,"=",b)
     masterpro.Params.OutputFlag = 0
     masterpro.optimize()
@@ -28,20 +28,29 @@ try:
     pricepro.setObjective(quicksum(x[i]*dual[i] for i in range(len(w))),sense=GRB.MAXIMIZE)
     pricepro.Params.OutputFlag = 0
     pricepro.optimize()
-    p = 0
+    p = len(w)-1
     while pricepro.objVal > 1:
         #print(masterpro.objVal," ",pricepro.objVal)
         p += 1
         #get a column for the new variable
         coeff =pricepro.getAttr("X",pricepro.getVars())
         column = Column(coeff,masterpro.getConstrs())
-        masterpro.addVar(obj=1.0,vtype=GRB.CONTINUOUS,name="new_patten_"+str(p),column=column)
+        masterpro.addVar(obj=1.0,vtype=GRB.CONTINUOUS,name="p_"+str(p),column=column)
         masterpro.optimize()
         #change variables' coeff of priceproblem in the objective function
         for i in range(len(w)):
             x[i].obj = set_partition[i].pi
         pricepro.optimize()
+    #after CG processe, we need to make the varibles in RMP to be Binary.
+    for v in masterpro.getVars():
+        v.setAttr("VType",GRB.BINARY)
+    masterpro.optimize()
     masterpro.write("master.lp")
-    print(masterpro.objVal,pricepro.objVal)
+    print("the total knapsacks need to use: ",masterpro.objVal)
+    solution = masterpro.getAttr("X",masterpro.getVars())
+    #print(solution)
+    for i in range(len(solution)):
+        if solution[i] > 0.5:
+            print(f"patten {i} is select")
 except GurobiError as e:
     print("Erro code "+str(e.errno)+":"+str(e))
